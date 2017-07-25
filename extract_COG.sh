6430e99784bfe413f7c2028ea56eb56f14bec5d8
raymondkiu <raymond.kiu@ifr.ac.uk>
2017-07-25 15:47:23 +0100

    Update extract_COG.sh

--- extract_COG.sh
+++ extract_COG.sh
@@ -1,8 +1,8 @@
-awk -F "\t" '{print $11}' *.faa.emapper.annotations > COG_unsorted
+awk -F "\t" '{print $11}' *.emapper.annotations > COG_unsorted
 sort COG_unsorted > COG_sorted
 
-for j in A B C D E F G H I J K L M N O P Q T U Y Z R S X;do grep $j COG_sorted|wc -l >> COG_num;done
-for j in A B C D E F G H I J K L M N O P Q T U Y Z R S X;do echo $j >> COG_let;done
+for j in A B C D E F G H I J K L M N O P Q T U Y Z R S;do grep $j COG_sorted|wc -l >> COG_num;done
+for j in A B C D E F G H I J K L M N O P Q T U Y Z R S;do echo $j >> COG_let;done
 
 paste COG_let COG_num > COG_final
 sed 's/\t/,/g' COG_final > COG_final.csv
