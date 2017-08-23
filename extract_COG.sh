f377b1c7ee919a42832eb21c9b7fde8be584187c
raymondkiu <raymond.kiu@ifr.ac.uk>
2017-08-23 12:26:35 +0100

    Update extract_COG.sh
    Add in fi command so script can be executed on specific annotated file

--- extract_COG.sh
+++ extract_COG.sh
@@ -1,14 +1,30 @@
-awk -F "\t" '{print $11}' *.emapper.annotations > COG_unsorted
-sort COG_unsorted > COG_sorted
+#!/bin/bash
+if [ $# -lt 1 ] ; then
+    echo ""
+    echo "usage: extract_COG.sh annotation_file.annotations ..|| *.emapper.annotations"
+    echo "sort COG classes"
+    echo ""
+    exit 0
+fi
 
-for j in A B C D E F G H I J K L M N O P Q T U Y Z R S W V;do grep $j COG_sorted|wc -l >> COG_num;done
-for j in A B C D E F G H I J K L M N O P Q T U Y Z R S W V;do echo $j >> COG_let;done
+filear=${@};
+for i in ${filear[@]}
 
-paste COG_let COG_num > COG_final
-sed 's/\t/,/g' COG_final > COG_final.csv
+do
 
-rm COG_let
-rm COG_num
-rm COG_final
-rm COG_sorted
-rm COG_unsorted
+awk -F "\t" '{print $11}' $i > $i-COG-unsorted
+sort $i-COG-unsorted > $i-COG-sorted
+
+for j in A B C D E F G H I J K L M N O P Q T U X Y Z R S W V;do grep $j $i-COG-sorted|wc -l >> $i-COG-num;done
+for j in A B C D E F G H I J K L M N O P Q T U X Y Z R S W V;do echo $j >> $i-COG-let;done
+
+paste $i-COG-let $i-COG-num > $i-COG-final
+sed 's/\t/,/g' $i-COG-final > $i-COG-final.csv
+
+rm $i-COG-let
+rm $i-COG-num
+rm $i-COG-final
+rm $i-COG-sorted
+rm $i-COG-unsorted
+
+done
